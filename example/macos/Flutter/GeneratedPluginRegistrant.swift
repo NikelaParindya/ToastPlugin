8e60243d400fd9059b62ea3e333953a821835b03
Nikela <it20300500@my.sliit.lk>
2023-04-06 11:35:32 +0530

    test

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import toastmessage
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ToastmessagePlugin.register(with: registry.registrar(forPlugin: "ToastmessagePlugin"))
+}
